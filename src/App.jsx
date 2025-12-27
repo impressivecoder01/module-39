9344f881ce1da46f79464b149aaed8ca7f8632d0
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-28 02:31:41 +0600

    class two

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Suspense } from 'react'
 import './App.css'
-import { add, diff, divide as div, multi } from './calculation/math/math'
-import { calculateTax } from './calculation/tax/tax'
+import Bottles from './Components/Bottles/Bottles'
+// import { add, diff, divide as div, multi } from './calculation/math/math'
+// import { calculateTax } from './calculation/tax/tax'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const sum = add(3,3)
-  console.log(sum)
-  const sub = diff(4,4)
-  console.log(sub)
-  const multiply = multi(4,4)
-  console.log(multiply)
-  const divide = div(4,4)
-  console.log(divide)
-  const a = calculateTax
-  console.log(a)
+  const bottlesPromise = fetch('./bottles.json')
+  .then(res => res.json())
+  
+  // const sum = add(3,3)
+  // console.log(sum)
+  // const sub = diff(4,4)
+  // console.log(sub)
+  // const multiply = multi(4,4)
+  // console.log(multiply)
+  // const divide = div(4,4)
+  // console.log(divide)
+  // const a = calculateTax
+  // console.log(a)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Suspense fallback={<h1>Bottle </h1>}>
+        <Bottles bottlesPromise={bottlesPromise}>
+
+        </Bottles>
+      </Suspense>
+      
     </>
   )
 }
